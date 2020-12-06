a4c9928233a47ab5a26f301a61a3a2fb32b189c5
arleyvelascoo <arley2180984@correo.uis.edu.co>
2020-12-06 15:42:57 -0500

    listo el taller 6

--- taller5js/main.js
+++ taller5js/main.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var no = document.getElementById('gridRadios2');
     no.checked = true;
     var yes = document.getElementById('gridRadios1');
     yes.checked = false;
     var gustoform = document.getElementById('gustos');
-    no.addEventListener('click', function(){
+    no.addEventListener('click', function () {
         gustoform.innerHTML = '';
     });
     yes.addEventListener('click', () => {
@@ -15,14 +15,42 @@ document.addEventListener("DOMContentLoaded", function() {
             '</div></div><div class="form-row"><div class="form-group col-md-6"><label for="inputAddress">Estilo de Carro Favorito</label>' +
             '<input type="text" class="form-control" id="style"></div><div class="form-group col-md-6"><label for="inputPassword4">Modelo del carro que más le gusta</label>' +
             '<input type="text" class="form-control" id="model"></div></div>' +
-	    '<div class="form-group">'+
-            '<label for="inputAddress">Rango de Precio para su vehiculo </label>' +
-            '<iframe id="ifram" style="border: 0;" class="col-md-12" src="1.html"></iframe>'+
-	    '</div>' +
-	''
-      ;
-      
+            '<div class="form-row">' +
+            '<label for="inputPassword4">Rango de precio dispuesto a pagar</label>' +
+            '<div class="double-slider">' +
+            '<input id="range_from" type="range" name="range_from" class="from" value="160" min="5" max="500" data-prev-value="30"><div class="progressbar_from"></div><input id="range_to" type="range" name="range_to" class="to" value="340" min="5" max="500" data-prev-value="350"><div class="progressbar_to"></div><span class="value-output from">160</span><span class="value-output to">340</span>' +
+            '</div></div>';
+        (function () {
+            // Get inputs by container
+            $('.double-slider input[type="range"]').on('input', function (e) {
+                // Split the elements From/To Slider and From/To values so you can handle them separtely 
+                var fromSlider = $(this).parent().children('input[type="range"].from'),
+                    toSlider = $(this).parent().children('input[type="range"].to'),
+                    fromValue = parseInt($(this).parent().children('input[type="range"].from').val()),
+                    toValue = parseInt($(this).parent().children('input[type="range"].to').val()),
+                    currentlySliding = $(this).hasClass('from') ? 'from' : 'to',
+                    outputElemFrom = $(this).parent().children('.value-output.from'),
+                    outputElemTo = $(this).parent().children('.value-output.to');
 
+                // Check which slider has been adjusted and prevent them from crossing each other 
+                if (currentlySliding == 'from' && fromValue >= toValue) {
+                    fromSlider.val((toValue - 1));
+                    fromValue = (toValue - 1);
+                } else if (currentlySliding == 'to' && toValue <= fromValue) {
+                    toSlider.val((fromValue + 1));
+                    toValue = (fromValue + 1);
+                }
+
+                // Updating the output values so they mirror the current slider's value
+                outputElemFrom.html(fromValue);
+                outputElemTo.html(toValue);
+
+                // Caluculating and setting the progressbar widths    
+                $('.progressbar_from').css('width', ((fromSlider.width() / parseInt(fromSlider[0].max)) * fromSlider[0].value) + "px");
+                $('.progressbar_to').css('width', (toSlider.width() - ((toSlider.width() / parseInt(toSlider[0].max)) * toSlider[0].value)) + "px");
+
+            });
+        })();
     });
 
     document.getElementById("formulario").addEventListener('submit', validarFormulario);
@@ -38,25 +66,25 @@ function validarFormulario(evento) {
         }
     }
 
-    function validarUsuario(campo){
-        charextrains = [" “ " , " ” " , " • " , " – " , " — " , " ˜ " , " ™ " , " š " , " › " , " œ " , " ž " , " Ÿ " , " Œ " , " ¡ " , " ¢ " , " £ " , " ¤ " , " ¥ " , " ¦ " , " § " , " ¨ " , " © " , " ª " , "@", "#"," « " , " ¬ " , " ­ " , " ® " , " ~ " , " Δ " , " € " , " ‚ " , " ƒ " , " „ " , " … " , " † " , " ‡ " , " ˆ " , " ‰ " , " Š " , " ‹ " , " ¯ " , " ° " , " ± " , " ² " , " ³ " , " ´ " , " µ " , " ¶ " , " · " , " ¸ " , " ¹ " , " º " , " » " , " ¼ " , " ½ " , " ¾ " , " ¿ " , " À " , " Á " , " Â " , " Ã " , " Ä " , " Å " , " Æ " , " Ç " , " È " , " É " , " Ê "];
+    function validarUsuario(campo) {
+        charextrains = [" “ ", " ” ", " • ", " – ", " — ", " ˜ ", " ™ ", " š ", " › ", " œ ", " ž ", " Ÿ ", " Œ ", " ¡ ", " ¢ ", " £ ", " ¤ ", " ¥ ", " ¦ ", " § ", " ¨ ", " © ", " ª ", "@", "#", " « ", " ¬ ", " ­ ", " ® ", " ~ ", " Δ ", " € ", " ‚ ", " ƒ ", " „ ", " … ", " † ", " ‡ ", " ˆ ", " ‰ ", " Š ", " ‹ ", " ¯ ", " ° ", " ± ", " ² ", " ³ ", " ´ ", " µ ", " ¶ ", " · ", " ¸ ", " ¹ ", " º ", " » ", " ¼ ", " ½ ", " ¾ ", " ¿ ", " À ", " Á ", " Â ", " Ã ", " Ä ", " Å ", " Æ ", " Ç ", " È ", " É ", " Ê "];
         var com = true;
         charextrains.forEach(element => {
-            element=element.trim()
+            element = element.trim()
             if (campo.includes(element)) {
                 com = false;
                 return;
             }
         })
-        return(validarLongitud(campo, 10, 20)&&com);
+        return (validarLongitud(campo, 10, 20) && com);
     }
 
     function validarEmail(campo) {
         var com = true;
-        if(!campo.includes('@')) {
+        if (!campo.includes('@')) {
             com = false;
         }
-        return(validarLongitud(campo, 6, 120)&&com);
+        return (validarLongitud(campo, 6, 120) && com);
     }
 
     function validarPassword(campo) {
@@ -78,12 +106,12 @@ function validarFormulario(evento) {
                 letter = true;
             }
         })
-        return(validarLongitud(campo, 15, 20) && /^[0-9A-Za-z/#%&]+$/.test(campo) && num && com && letter);
+        return (validarLongitud(campo, 15, 20) && /^[0-9A-Za-z/#%&]+$/.test(campo) && num && com && letter);
     }
 
-    function validarDireccion(campo){
-        var com=false;
-        validos=['cll', 'cra', 'av', 'anv', 'trans']
+    function validarDireccion(campo) {
+        var com = false;
+        validos = ['cll', 'cra', 'av', 'anv', 'trans']
         validos.forEach(element => {
             if (campo.toLowerCase().startsWith(element.toLowerCase())) {
                 com = true;
@@ -93,33 +121,34 @@ function validarFormulario(evento) {
     }
 
     evento.preventDefault();
+
     var alerta = document.getElementById('alerta');
     var message;
     var firstName = document.getElementById('inputFirstname').value;
-    if(!validarLongitud(firstName, 1, 25)) {
+    if (!validarLongitud(firstName, 1, 25)) {
         message = "El nombre no es valido, debe tener minimo 1 carater y maximo 25";
-        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> '+message +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
+        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> ' + message + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
         window.scroll(0, 0);
         return;
     }
     var lastName = document.getElementById('inputLastName').value;
     if (!validarLongitud(lastName, 1, 25)) {
         message = "El apellido no es valido, debe tener minimo 1 carater y maximo 25";
-        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> '+message +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
+        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> ' + message + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
         window.scroll(0, 0);
         return;
     }
     var user = document.getElementById('inputUser').value;
     if (!validarUsuario(user)) {
         message = "El usuario no debe contener caracteres raros, debe tener mínimo 10 caracteres y máximo 20";
-        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> '+message +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
+        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> ' + message + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
         window.scroll(0, 0);
         return;
     }
     var email = document.getElementById('inputEmail4').value;
     if (!validarEmail(email)) {
         message = "No es una dirección de correo valida";
-        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> '+message +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
+        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> ' + message + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
         window.scroll(0, 0);
         return;
     }
@@ -127,23 +156,23 @@ function validarFormulario(evento) {
     var password = document.getElementById('inputPassword4').value;
     if (!validarPassword(password)) {
         message = "Verifica que hayas ingresado un password correcto";
-        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> '+message +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
+        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> ' + message + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
         window.scroll(0, 0);
         return;
     }
 
     var confirmpassword = document.getElementById('inputPassword5').value;
-    if (password!=confirmpassword){
+    if (password != confirmpassword) {
         message = "Las contraseñas no coinciden";
-        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> '+message +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
+        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> ' + message + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
         window.scroll(0, 0);
         return;
     }
 
     var direccion = document.getElementById('inputAddress').value;
-    if (!validarDireccion(direccion)){
+    if (!validarDireccion(direccion)) {
         message = "La dirección debe empezar por Av/Cll/Cra/Anv/Trans";
-        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> '+message +'<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
+        alerta.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"> <strong>Atención! </strong> ' + message + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>';
         window.scroll(0, 0);
         return;
     }
---
+++ taller6jsjquery/main.js
@@ -0,0 +1,378 @@
+$(document).ready(() => {
+	// inicializando cosas de materialize
+	$('.sidenav').sidenav();
+	$('.tabs').tabs();
+	$('.datepicker').datepicker();
+
+	//funcionalidad de formulario y grilla
+	let res = $('#main')
+
+	//función click para menú integrantes
+	$('#boton0').on('click', () => {
+		var resp = `
+					<div class="col s12 center-align">
+						<h2>Members</h2>
+					</div>
+
+					<div class="row">
+						<div class="col s6">
+							<div class="card">
+								<div class="card-image waves-effect waves-block waves-light">
+									<img class="activator" src="imgs/foto2.jpg">
+								</div>
+								<div class="card-content">
+									<span class="card-title activator grey-text text-darken-4">Arley David Velasco-2180984<i
+												class="material-icons right">more_vert</i></span>
+									<p><a class="waves-effect waves-light btn-small blue-grey darken-4" href="https://www.instagram.com/arleyvelascoo/">View more</a></p>
+								</div>
+								<div class="card-reveal">
+									<span class="card-title grey-text text-darken-4">Description<i
+												class="material-icons right">close</i></span>
+									<p>Sixth semester student of Systems Engineering at the Industrial University of Santander.
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col s6">
+							<div class="card">
+								<div class="card-image waves-effect waves-block waves-light">
+									<img class="activate" src="imgs/i3.jpeg">
+								</div>
+								<div class="card-content">
+									<span class="card-title activator grey-text text-darken-4">Horacio Antonio Camacho-2180986<i
+												class="material-icons right">more_vert</i></span>
+									<p><a class="waves-effect waves-light btn-small blue-grey darken-4" href="https://www.linkedin.com/in/horacio-antonio-camacho-holguin-a219a5168/">View
+										more</a></p>
+								</div>
+								<div class="card-reveal">
+									<span class="card-title grey-text text-darken-4">Description<i
+												class="material-icons right">close</i></span>
+									<p>Sixth semester student of Systems Engineering at the Industrial University of Santander.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>`;
+
+		res.html(resp);
+	});
+
+	//funcion click para menú formulario
+	$('#boton1').on('click', () => {
+		var html = `
+		<h3 class="center-align">CONTACTO</h3>
+				<form id="formulario" action="https://wwww.facebook.com">
+					<div class="row">
+						<div class="input-field col s6">
+							<i class="material-icons prefix">perm_identity</i>
+							<label for="nombre">Nombre</label>
+							<input type="text" name="nombre" required>
+						</div>
+						<div class="input-field col s6">
+							<i class="material-icons prefix">person_pin</i>
+							<label for="apellido">Apellido</label>
+							<input type="text" name="apellido" required>
+						</div>					
+					</div>	
+
+					<div class="row">
+						<div class="input-field col s6">
+							<i class="material-icons prefix">date_range</i>
+							<label for="apellido">Nacimiento</label>	<input type="text" class="datepicker" id="fecha_nacimiento" required> </div>
+						<div class="input-field col s6">
+							<i class="material-icons prefix">check</i>
+							<label class="active" for="apellido">Edad</label>	
+							<input type="text" class="" id="age" readonly value="-"> 
+						</div>
+					</div>				
+
+					<div class="row">
+						<div class="input-field col s6">
+							<i class="material-icons prefix">email</i>
+							<label for="email">Email</label>
+							<input type="email" name="email" required>
+						</div>	
+						<div class="input-field col s6">
+							<i class="material-icons prefix">assignment_ind</i>
+							<label for="user">Usuario</label>
+							<input type="text" id="user" required>
+						</div>	
+					</div>
+
+					<div class="row">
+						<div class="input-field col s6">
+							<i class="material-icons prefix">password</i>
+							<label for="password">Contraseña</label>
+							<input type="password" id="password" required>
+						</div>
+						<div class="input-field col s6">
+							<i class="material-icons prefix">password</i>
+							<label for="password2">Confirmar</label>
+							<input type="password" id="password2" required>
+						</div>
+					</div>
+
+					<div class="row">
+					<div class="input-field col s12">
+						<select id="enfermedades">
+							<option value="" disabled selected>Choose your option</option>
+							<option value="yes">Sí</option>
+							<option value="no">No</option>
+						</select>
+						<label>¿Ha sufrido enfermedades contagiosas?</label>
+  				</div>
+  				</div>				
+					<div class="row" id="input_enfermedades">
+						<div class="input-field col s12">
+							<i class="material-icons prefix">mode_edit</i>
+							<textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+							<label for="icon_prefix2">¿Cuales?. Escribalas a continuación</label>
+						</div>
+					</div>
+					<p class="center-align">
+						<button class="waves-effect waves-light btn amber accent-4 blue-grey-text text-darken-4" type="submit"><i class="material-icons right">send</i>enviar</button>
+					</p>
+
+				</form>
+		`
+		res.html(html)
+		$('.datepicker').datepicker({
+			i18n: {
+				months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+				monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Dic"],
+				weekdays: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+				weekdaysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+				weekdaysAbbrev: ["D", "L", "M", "M", "J", "V", "S"]
+			},
+			selectMonths: true, // Creates a dropdown to control month
+			selectYears: 100, // Creates a dropdown of 15 years to control year
+			format: 'yyyy/mm/dd',
+			maxDate: new Date()
+		});
+		$('select').formSelect();
+
+		$('.datepicker-done').on('click', () => {
+
+			function calcularEdad(fecha) {
+				var hoy = new Date();
+				var cumpleanos = new Date(fecha);
+				var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+				var m = hoy.getMonth() - cumpleanos.getMonth();
+				if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+					edad = edad - 1;
+				}
+				return edad;
+			}
+			$('#age').val(calcularEdad($('#fecha_nacimiento').val()))
+		})
+
+
+		$('#fecha_nacimiento').on('focus', function () {
+			$(this).click()
+		})
+
+		$("#formulario").on('submit', validarFormulario);
+
+		function validarFormulario(evento) {
+
+			function validarLongitud(campo, minlength, maxlength) {
+				if (campo.length >= minlength && campo.length <= maxlength) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			function validarUsuario(campo) {
+				charextrains = [" “ ", " ” ", " • ", " – ", " — ", " ˜ ", " ™ ", " š ", " › ", " œ ", " ž ", " Ÿ ", " Œ ", " ¡ ", " ¢ ", " £ ", " ¤ ", " ¥ ", " ¦ ", " § ", " ¨ ", " © ", " ª ", "@", "#", " « ", " ¬ ", " ­ ", " ® ", " ~ ", " Δ ", " € ", " ‚ ", " ƒ ", " „ ", " … ", " † ", " ‡ ", " ˆ ", " ‰ ", " Š ", " ‹ ", " ¯ ", " ° ", " ± ", " ² ", " ³ ", " ´ ", " µ ", " ¶ ", " · ", " ¸ ", " ¹ ", " º ", " » ", " ¼ ", " ½ ", " ¾ ", " ¿ ", " À ", " Á ", " Â ", " Ã ", " Ä ", " Å ", " Æ ", " Ç ", " È ", " É ", " Ê "];
+				var com = true;
+				charextrains.forEach(element => {
+					element = element.trim()
+					if (campo.includes(element)) {
+						com = false;
+						return;
+					}
+				})
+				return (com);
+			}
+
+			function validarPassword(campo) {
+				var com = true;
+				if (campo == campo.toLowerCase()) {
+					com = false;
+				}
+				const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+				var num = false;
+				numbers.forEach(elemento => {
+					if (campo.includes(elemento)) {
+						num = true;
+					}
+				})
+				var letter = false;
+				const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+				letras.forEach(elemento => {
+					if (campo.includes(elemento)) {
+						letter = true;
+					}
+				})
+				return (validarLongitud(campo, 15, 20) && /^[0-9A-Za-z/#%&]+$/.test(campo) && num && com && letter);
+			}
+
+			evento.preventDefault();
+
+			const toast = (message) => {
+				M.toast({
+					html: message,
+					classes: 'amber accent-4 black-text'
+				})
+			}
+			var message;
+			var user = $('#user');
+			if (!validarUsuario(user.val())) {
+				message = "El usuario no debe contener caracteres raros";
+				toast(message)
+				window.scroll(0, 0);
+				user.focus()
+				return;
+			}
+
+			var password = $('#password');
+			if (!validarPassword(password.val())) {
+				message = "Verifica que hayas ingresado un password correcto, Mínimo 15 caracteres, máximo 20. Debe contener letras, mayusculas, números y/o [#, /, %, $]";
+				toast(message)
+				window.scroll(0, 0);
+				password.focus()
+				return;
+			}
+
+			var confirmpassword = $('#password2');
+			if (password.val() != confirmpassword.val()) {
+				message = "Las contraseñas no coinciden";
+				toast(message)
+				window.scroll(0, 0);
+				confirmpassword.focus()
+				return;
+			}
+
+			this.submit();
+		}
+
+
+		$('#input_enfermedades').hide();
+		$('#enfermedades').on('change', function () {
+			if ($(this).val() == 'yes') {
+				$('#input_enfermedades').show(300);
+				console.log('vamos a mostrar una selección de enfermedades')
+			} else {
+				$('#input_enfermedades').hide(300);
+				console.log('No vamos a mostrar una selección de enfermedades')
+			}
+		})
+	});
+
+	//funcion click para menú grilla jquery
+	$('#boton2').on('click', () => {
+		$.ajax({
+			// la URL para la petición
+			url: 'https://jsonplaceholder.typicode.com/users',
+
+			// la información a enviar
+			// (también es posible utilizar una cadena de datos)
+
+			//data: {id: 123},
+
+			// especifica si será una petición POST o GET
+			type: 'GET',
+
+			// el tipo de información que se espera de respuesta
+			dataType: 'json',
+
+			// código a ejecutar si la petición es satisfactoria;
+			// la respuesta es pasada como argumento a la función
+			success: function (json) {
+				var html = "";
+
+				html += `
+				<div class="container">
+					<div class="input-field">
+						<i class="material-icons prefix">search</i>
+						<input id="search" class="holamundoinput " type="search" required>
+						<label>Busca </label>
+					</div>
+        </div>
+				<table id="myTable">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Nombre</th>
+							<th>Usuario</th>
+							<th>Email</th>
+							<th>Dirección</th>
+							<th>Telefono</th>
+							<th>Sitio web</th>
+							<th>Compañía</th>
+						</tr>
+					</thead>
+					<tbody>`
+				for (let item of json) {
+					html += `<tr>
+						<td> ${item.id} </td>
+						<td> ${item.name} </td>
+						<td>${item.username}</td>
+						<td>${item.email}</td>
+						<td>${item.address.street + " " + item.address.suite}</td>
+						<td>${item.phone}</td>
+						<td>${item.website}</td>
+						<td>${item.company.name + " " + item.company.catchPhrase}</td>
+						</tr>`
+				}
+				html += `</tbody>
+				</table>`
+				//Asigneramos el listener para poder hacer el filtrado con keyup
+				res.html(html)
+				$('#search').on('keyup', function () {
+					var input = $(this).val().toLowerCase();
+					$("#myTable tbody tr").filter(function () {
+						$(this).toggle($(this).text().toLowerCase().indexOf(input) > -1)
+					});
+				});
+			}
+
+			// código a ejecutar si la petición falla;
+			// son pasados como argumentos a la función
+			// el objeto de la petición en crudo y código de estatus de la petición
+			//error: function (xhr, status) {
+			//	alert('Disculpe, existió un problema');
+			//},
+
+			// código a ejecutar sin importar si la petición falló o no
+			//complete: function (xhr, status) {
+			//alert('Petición realizada');
+			//}
+		})
+
+	});
+	$('#boton').on('click', () => {
+		const xhttp = new XMLHttpRequest();
+
+		xhttp.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+
+		xhttp.send();
+
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				var objeto = JSON.parse(this.responseText);
+				console.log(objeto);
+
+				let res = $('#res');
+				res.html('');
+
+				for (let item of objeto) {
+					res.append('<tr><td>' + item.id + '</td><td>' + item.name + '</td></tr>')
+				}
+			}
+		}
+	});
+
+});
+
